f5dbb1e0ff6f70bcbfbf1ba150e9ec78a7e4a8f5
Kapil Amagain <kapil.amagain@gmail.com>
2019-07-07 19:43:37 +1000

    FeedTableView Setup ongoing

--- InstagramApp/Views/FeedTableViewCell.swift
+++ InstagramApp/Views/FeedTableViewCell.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
     
-
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameTItleButton: UIButton!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var likesCountLabel: UILabel!
+    @IBOutlet weak var postCommentLabel: UILabel!
+    @IBOutlet weak var commentCountButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        profileImage.layer.cornerRadius = profileImage.frame.width / 2
+        profileImage.layer.masksToBounds = true
+        selectionStyle = UITableViewCell.SelectionStyle.none
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
--- InstagramApp/Views/StoryCollectionViewCell.swift
+++ InstagramApp/Views/StoryCollectionViewCell.swift
@@ -15,7 +15,10 @@ class StoryCollectionViewCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        storyImage.layer.cornerRadius = storyImage.frame.width / 2
+        storyImage.layer.masksToBounds = true
+        storyImage.layer.borderColor = UIColor.white.cgColor
+        storyImage.layer.borderWidth = CGFloat(3.0)
     }
 
 }
