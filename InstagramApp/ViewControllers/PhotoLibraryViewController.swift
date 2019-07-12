ee1a80290ed4989c93750afef14454cdea7eb52c
Kapil Amagain <kapil.amagain@gmail.com>
2019-07-12 20:40:54 +1000

    Photo Library Work in Progress

--- InstagramApp/ViewControllers/PhotoLibraryViewController.swift
+++ InstagramApp/ViewControllers/PhotoLibraryViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoLibraryViewController: UIViewController {
-
+    
+    @IBOutlet weak var photosCollectionView: UICollectionView!
+    
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        photosCollectionView.delegate = self
+        photosCollectionView.dataSource = self
+        populatePhotos()
     }
+    
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization{ [weak self] status in
+            if status == .authorized {
+                //access the photos from photo library
+                let assets = PHAsset.fetchAssets(with: .image, options: nil)
+                assets.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                    self?.images.reverse()
+                    DispatchQueue.main.async {
+                        self?.photosCollectionView.reloadData()
+                    }
+                }
+            }
+        }
+    }
+}
 
+extension PhotoLibraryViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCollectionViewCell else  {
+            fatalError("CollectionView Cell not found")
+        }
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+        }
+        return cell
+    }
+}
+
+extension PhotoLibraryViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+extension PhotoLibraryViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
 }
---
+++ InstagramApp/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  InstagramApp
+//
+//  Created by User on 12/7/19.
+//  Copyright © 2019 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
