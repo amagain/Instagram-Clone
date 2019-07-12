62aa51ff54e4c56eb131b1b2b9b492dd9b41a018
Kapil Amagain <kapil.amagain@gmail.com>
2019-07-12 19:12:37 +1000

    NewPostPageViewController

--- InstagramApp/ViewControllers/NewPostPageViewController.swift
+++ InstagramApp/ViewControllers/NewPostPageViewController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 
-class NewPostPageViewController: UIPageViewController {
-
+class NewPostPageViewController: UIPageViewController, UIPageViewControllerDelegate {
+    
+    var orderedViewControllers: [UIViewController] = [UIViewController]()
+    var pagesToShow: [NewPostPagesToShow] = NewPostPagesToShow.PagesToShow()
+    var currentIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        delegate = self
     }
     
-
-
-
+    private func newViewController(pageToShow: NewPostPagesToShow) -> UIViewController {
+        var viewController: UIViewController!
+        let newPostStoryboard = UIStoryboard(name: "NewPost", bundle: nil)
+        
+        switch pageToShow {
+        case .camera:
+            viewController = newPostStoryboard.instantiateViewController(withIdentifier: pageToShow.identifier) as! CameraViewController
+        case .library:
+            viewController = newPostStoryboard.instantiateViewController(withIdentifier: pageToShow.identifier) as! PhotoLibraryViewController
+        }
+        return viewController
+    }
+    
+    func showViewController(index: Int) {
+        if currentIndex > index {
+            setViewControllers([orderedViewControllers[index]], direction: UIPageViewController.NavigationDirection.reverse, animated: true, completion: nil)
+        }
+        else if currentIndex < index {
+            setViewControllers([orderedViewControllers[index]], direction: UIPageViewController.NavigationDirection.forward, animated: true, completion: nil)
+        }
+        currentIndex = index
+    }
 }
