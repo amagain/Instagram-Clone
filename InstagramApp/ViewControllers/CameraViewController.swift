0ae6f02ce1f67ae423bfaee8816a836e6276a070
Kapil Amagain <kapil.amagain@gmail.com>
2019-07-12 18:33:11 +1000

    New Post Storyboard Setup

---
+++ InstagramApp/ViewControllers/CameraViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CameraViewController.swift
+//  InstagramApp
+//
+//  Created by User on 12/7/19.
+//  Copyright © 2019 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ InstagramApp/ViewControllers/NewPostPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewPostPageViewController.swift
+//  InstagramApp
+//
+//  Created by User on 12/7/19.
+//  Copyright © 2019 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class NewPostPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+
+}
